1f9db22cdc9c4486b45306164e111b4bfc14d521
natasha <n.kasapenko@mail.ru>
2020-10-31 19:24:01 +0300

    fixed, added file type

--- task1.c
+++ task1.c
@@ -2,6 +2,12 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <stdlib.h>
+#include <sys/sysmacros.h>
+
+#define S_BLKSIZE 512
 
 int main (int argc, char *argv[])
 {
@@ -17,13 +23,32 @@ int main (int argc, char *argv[])
 	{
 		perror("Failed to stat");
 		return 2;
-	}
+	}                                        
 
-	printf("Mode: %06o \n Size: %llu \n Used space: %llu \n User_id: %llu\n Group_id: %llu\n",
+    printf("Mode: %06o \nSize: %llu \nUsed space: %llu \nUser_id: %u\nGroup_id: %u\n",
 			(unsigned int) stat_buf.st_mode, 
-			(unsigned long long) stat_buf.st_size, 
-			(unsigned long long) stat_buf.st_blocks * 512,
-			(unsigned long long) stat_buf.st_uid,
-			(unsigned long long) stat_buf.st_gid);
-}
+			(long long unsigned) stat_buf.st_size, 
+			(long long unsigned) stat_buf.st_blocks * S_BLKSIZE,
+			(unsigned int) stat_buf.st_uid,
+			(unsigned int) stat_buf.st_gid);
 
+     printf("File type:");
+     switch (stat_buf.st_mode & S_IFMT)
+     {
+         case S_IFBLK: printf("block device\n"); 
+                    break;
+         case S_IFCHR: printf("character device\n");
+                    break;
+         case S_IFDIR: printf("directory\n");
+                    break;
+         case S_IFIFO: printf("FIFO/pipe\n");
+                    break;
+         case S_IFLNK: printf("symlink\n");
+                    break;
+         case S_IFREG: printf("regular file\n");
+                    break;
+         case S_IFSOCK: printf("socket\n");
+                    break;
+         default: printf("inknown type\n");   
+     }
+}
