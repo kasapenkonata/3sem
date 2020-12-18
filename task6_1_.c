4256f906d10b79108e5c1a0574ba00c83670b289
natasha <n.kasapenko@mail.ru>
2020-12-18 22:50:39 +0300

    fixed 6_1, added printf dir type using pdir or lstat

--- task6_1_.c
+++ task6_1_.c
@@ -5,12 +5,46 @@
  #include <sys/types.h>
  #include <sys/stat.h>
 
+char* dir_type(int type)
+{
+	switch (type)
+	{
+		case DT_FIFO:   return "FIFO";                  break;
+		case DT_CHR:    return "CHARACTER SPECIAL";     break;
+		case DT_DIR:    return "DIRECTORY";             break;
+		case DT_BLK:    return "BLOCK SPECIAL";         break;
+		case DT_REG:    return "REGULAR";               break;
+		case DT_LNK:    return "SYMBOLIC LINK";         break;
+		case DT_SOCK:   return "SOCKET";                break;
+		case DT_WHT:    return "WHITEOUT";              break;
+		default: return "unknown?";                     break;
+	}
+}
+
+char* get_type(struct stat sb)
+{
+	switch (sb.st_mode & S_IFMT)
+	{
+		case S_IFBLK:  return "block device"; 
+		case S_IFCHR:  return "character device";
+		case S_IFDIR:  return "directory";
+		case S_IFIFO:  return "FIFO/pipe";
+		case S_IFLNK:  return "symlink";
+		case S_IFREG:  return "regular file";
+		case S_IFSOCK: return "socket";
+		default:       return "unknown?";
+	}
+	return 0;
+}
+
 int main (int argc, char *argv[]) 
 	{
 	       	struct dirent *pDirent;
             	DIR *pDir;
             	char *dirName;
            	struct stat sb;
+		int result = 0;
+
             	if (argc == 1) 
 		{
                 	dirName = ".";
@@ -24,6 +58,7 @@ int main (int argc, char *argv[])
               		}
             	dirName = argv[1];
 	        }
+
            	pDir = opendir (dirName);
            	if (pDir == NULL) 
 		{
@@ -32,17 +67,36 @@ int main (int argc, char *argv[])
 	        }
 		
 	   	printf("Entire directory %s:\n", dirName);
-	   	printf("%30s%15s%20s\n", "Name", "Type", "Size");
+	   	printf("%30s %15s\n",  "Type", "Name");
+
 	   	while ((pDirent = readdir(pDir)) != NULL) 
 		{
 	  		 if (lstat(pDirent->d_name, &sb) == -1)
 			 {
 	      			printf ("Failed to stat %s\n%s\n", pDirent->d_name, strerror(errno));
 	              		return 3;
-	           	 }  
-	        	 printf ("%30s%15d%20u bytes\n", pDirent->d_name, pDirent->d_type, (unsigned)sb.st_size);
+	           	 }
+			
+			 if (pDirent->d_type == DT_UNKNOWN)
+			 {
+				printf ("%30s %15s\n", get_type(sb), pDirent->d_name);
+			 }
+			 else 
+			 {
+				 if (lstat(pDirent->d_name, &sb) == -1)
+				 {
+					 printf ("Failed to stat %s\n%s\n", pDirent->d_name, strerror(errno));					
+					 return 3;
+				 }					 
+				 printf ("%30s %15s\n", dir_type(pDirent->d_type), pDirent->d_name);
+			 }
 		}
 
-	        closedir (pDir);
+	        result = closedir (pDir);
+		if (result == -1)
+		{
+			perror("Unable to close dir");
+			return 3;
+		}
 	        return 0;
 	}
