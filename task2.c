8b806649dd5293db84159d56151692bb4c2a3239
natasha <n.kasapenko@mail.ru>
2020-12-18 15:03:20 +0300

    fixed task2.c

--- task2.c
+++ task2.c
@@ -1,53 +1,52 @@
-/**
- * task 2 - open, read, write, close*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <errno.h>
 #include <sys/types.h>
 #include <unistd.h>
- 
-#define BUF_SIZE 100
- 
-int main (int argc, char* argv[]) 
-{
-	 int input_fd, output_fd;  
-         ssize_t ret_in, ret_out; //ret_in - amount of read char, ret_out - amount of written char 
-	 char buffer[BUF_SIZE];  
-		     
-		   
-	 if(argc != 3) 
-	 {
-		 printf ("Usage: cp read_file_name write_file_name\n");
-		 return 1;
-	 }
-			 
-	 input_fd = open (argv [1], O_RDONLY);
-         if (input_fd == -1) 
-	 {
-		printf ("Can't open file for reading %s\n", argv [1]);
-		return 2;
-	 }
-				 
-	 output_fd = open(argv[2], O_WRONLY | O_CREAT, 0644);
-	 if(output_fd == -1)
-	 {
-	 	printf ("Can't create file for whiting %s\n", argv [2]);
-	 	return 3;
-	 }
+#include <errno.h>
+#include <string.h>
 
-	 while ((ret_in = read (input_fd, &buffer, BUF_SIZE)) > 0) 
-	 {
-		ret_out = write (output_fd, &buffer, (ssize_t) ret_in);
-		if (ret_out != ret_in) 
-		{
-		printf ("Error during copy process\n");
-		return 4;							     
-		}
-	 }
-					     
-		close (input_fd);
-		close (output_fd);
-						     
-		return 0;
+int main(int argc, char* argv[]) 
+{
+	int output_fd;
+	char* message;
+	int message_len;
+	size_t result;
+
+	if (argc != 3) 
+	{
+		printf("Usage: file_name string\n");
+		return 1;
 	}
+
+	output_fd = open(argv[1], O_WRONLY | O_CREAT, 0644);
+	if (output_fd == -1)
+	{
+		printf("Can't create file for writing");
+		return 2;
+	}
+
+	message_len = strlen(argv[2]);
+
+	for (size_t i = 0; i < message_len; i++) 
+	{
+		result = write(output_fd, &argv[2][i], 1);
+		if (result == -1)
+		{
+			perror("unable to write");
+			close(output_fd);
+			return 3;
+		}
+	}
+
+	result = close(output_fd);
+	if (result == -1)
+	{
+		perror("unable to close fd");
+		close(output_fd);
+		return 4;
+	}
+
+	return 0;
+}
