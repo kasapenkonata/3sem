fa72189d85799cf1ea08a571ab7a3ed0d5a83769
natasha <n.kasapenko@mail.ru>
2020-12-18 15:05:34 +0300

    task2_1. changed

--- task2_1.c
+++ task2_1.c
@@ -12,6 +12,7 @@ int main(int argc, char* argv[])
 	int output_fd;
 	char* message;
 	int message_len;
+	int result = 0;
 
 	if (argc != 3) 
 	{
@@ -30,10 +31,21 @@ int main(int argc, char* argv[])
 	
 	for (size_t i = 0; i < message_len; i++) 
 	{
-		dprintf(output_fd, "%c", argv[2][i]);
+		result = dprintf(output_fd, "%c", argv[2][i]);
+		if (result == -1)
+		{
+			perror("Unable to read");
+			close(output_fd);
+			return 3;
+		}
 	}
 
-	close(output_fd);
+	result = close(output_fd);
+	if (result == -1)
+	{
+		perror("Unable to close fd");
+		return 4;
+	}
 
 	return 0;
 }
