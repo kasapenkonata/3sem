b75ec37897225fbee5cbe40b07e120cdfb16c8a9
natasha <n.kasapenko@mail.ru>
2020-12-18 15:39:00 +0300

    fixed task3_1.c

--- task3_1.c
+++ task3_1.c
@@ -1,6 +1,3 @@
-/*
- * task 3.1 - open, read, write, close, checks if file is regular*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -11,14 +8,16 @@
 #include <errno.h>
 #include <string.h>
  
-#define BUF_SIZE 128
+#define BUF_SIZE 1024
  
 int main(int argc, char* argv[])
 {
 	int input_fd, output_fd;   
 	ssize_t ret_in, ret_out;   
-	char buffer[BUF_SIZE], buffer_copy[BUF_SIZE];
+	char buffer[BUF_SIZE];
 	struct stat stat_buf;
+	int result;
+
 	if(argc != 3)
 	{
 		printf ("Usage: cp read_file_name write_file_name\n");
@@ -30,37 +29,62 @@ int main(int argc, char* argv[])
 		return 2;
 	}
 
-	if (S_ISREG(stat_buf.st_mode) == 0)
+	if (!S_ISREG(stat_buf.st_mode))
 	{
-		printf("%s is not simple file", argv[1]);
+		printf("%s is not regular file", argv[1]);
 		return 3;
 	}
 	
 	input_fd = open (argv [1], O_RDONLY);
+
 	if (input_fd == -1) 
 	{
 		printf ("Can't open file for reading %s\n%s\n", argv [1], strerror(errno));
 		return 5;
 	}
-						   
-	output_fd = open(argv[2], O_WRONLY | O_CREAT);
+
+	output_fd = open(argv[2], O_WRONLY | O_CREAT, 00700);
+
 	if(output_fd == -1)
 	{
 		printf ("Can't create file for whiting %s\n%s\n", argv [2], strerror(errno));
+		close(input_fd);
 		return 6;
 	}
 
-	while((ret_in = read (input_fd, &buffer, BUF_SIZE)) > 0)
+	do 
 	{
-		ret_out = write (output_fd, &buffer, (ssize_t) ret_in);
-		if(ret_out != ret_in) 
+		ret_in = read (input_fd, &buffer, BUF_SIZE);
+ 		if (ret_in == -1)
 		{
-			printf ("Error during copy process\n%s\n", strerror(errno));
-			return 7;																     
+			perror("Unable to read");
+			close(input_fd);
+			close(output_fd);
+			return 7;
 		}
-	}	
-		close (input_fd);
-		close (output_fd);
+		ret_out = write (output_fd, &buffer, (ssize_t) ret_in);
+		if(ret_out != ret_in)
+	       	{
+			printf ("Error during copy process\n%s\n", strerror(errno));
+			return 8;
+		}
+
+	}
+	while (ret_in != 0);	
+
+	result = close(input_fd);
+	if (result == -1)
+	{
+		perror("unable to close input_fd");
+		return 9;
+	}
+
+	result = close(output_fd);
+	if (result == -1)
+	{
+		perror("unable to close output_fd");
+		return 10;
+	}
 
 	return 0;
 }
